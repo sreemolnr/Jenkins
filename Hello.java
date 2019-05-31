75a8380942403deb85f4db514da9d2b0e104e931
sreemolnr <50658376+sreemolnr@users.noreply.github.com>
2019-05-31 11:58:01 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello{
            for(int i=1;i<=10;i++){
    
                System.out.println("Hello world...");
-                 System.out.println(i);
+                 //System.out.println(i);
 
                }
      
